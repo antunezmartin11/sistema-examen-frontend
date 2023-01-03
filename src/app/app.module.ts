a355675a07cdbfd4c426e62aedeaafb9eddcf898
antunezmartin11 <antunezmartin@outlook.com>
2023-01-03 16:21:36 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button'
+import {MatInputModule} from '@angular/material/input';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+    HttpClientModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,48 @@
+import { UserService } from './../../services/user.service';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+
+  constructor(private userService:UserService, private snack:MatSnackBar) { }
+
+  public user = {
+    username:'',
+    password:'',
+    nombre:'',
+    apellidos:'',
+    email:'',
+    telefono:''
+  }
+  ngOnInit(): void {
+  }
+
+  formSubmint(){
+    console.log(this.user);
+    if(this.user.username=='' || this.user.username==null){
+      this.snack.open('El nombre de usuario es requerido !!','Aceptar',{
+        duration: 3000,
+        verticalPosition: 'top',
+        horizontalPosition: 'right'
+      });
+      return;
+    }
+    this.userService.añadirUsuario(this.user).subscribe((data)=>{
+      console.log(data)
+      alert('Usuario guardado con exito')
+      Swal.fire('Usuario registrado','Usuario registrado correctamente','success')
+    },(error)=>{
+      console.log(error)
+      this.snack.open('Ocurrio un error en el registro !!','Aceptar',{
+        duration: 3000
+      })
+    })
+  }
+}
